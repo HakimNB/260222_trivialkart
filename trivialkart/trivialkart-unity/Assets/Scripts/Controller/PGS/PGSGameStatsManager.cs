7ee525ddf10c8d51899d7768302b73f137556cfb
Venkat <wickedcube@gmail.com>
2026-04-16 15:10:06 +0530

    Updated Events

--- trivialkart/trivialkart-unity/Assets/Scripts/Controller/PGS/PGSGameStatsManager.cs
+++ trivialkart/trivialkart-unity/Assets/Scripts/Controller/PGS/PGSGameStatsManager.cs
@@ -8,27 +8,24 @@ public class PGSGameStatsManager : MonoBehaviour
     public void SingleEventLog()
     {
         Debug.Log("Recording single event...");
-        PlayerGameEvent playerGameEvent = new PlayerGameEvent.Builder("event_Single")
-            .AddProperty("Dist", GameDataController.GetGameData().distanceTraveled)
+        PlayerGameEvent singleEvent = new PlayerGameEvent.Builder("event_single")
+            .AddProperty("score_event", GameDataController.GetGameData().distanceTraveled)
             .Build();
-        PlayGamesPlatform.Instance.RecordEvent(playerGameEvent);
+        PlayGamesPlatform.Instance.RecordEvent(singleEvent);
     }
 
     public void MultiEventLog()
     {
         Debug.Log("Recording multiple events...");
-        List<PlayerGameEvent> events = new List<PlayerGameEvent>
+        List<PlayerGameEvent> multipleEvents = new List<PlayerGameEvent>
         {
-            new PlayerGameEvent.Builder("event_multiple_1")
-                .AddProperty("Dist", GameDataController.GetGameData().distanceTraveled)
+            new PlayerGameEvent.Builder("event_multiple_01")
+                .AddProperty("score_event", GameDataController.GetGameData().distanceTraveled)
                 .Build(),
-            new PlayerGameEvent.Builder("event_multiple_2")
-                .AddProperty("Coins", GameDataController.GetGameData().coinsOwned)
-                .Build(),
-            new PlayerGameEvent.Builder("event_multiple_3")
-            .AddProperty("SedanUnlocked", GameDataController.GetGameData().carIndexToOwnership[0] == Ownership.Owned)
-            .Build()
+            new PlayerGameEvent.Builder("event_multiple_02")
+                .AddProperty("run_time_event", GameDataController.GetGameData().coinsOwned)
+                .Build()
         };
-        PlayGamesPlatform.Instance.RecordEvents(events);
+        PlayGamesPlatform.Instance.RecordEvents(multipleEvents);
     }
-}
+}	
